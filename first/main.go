5406f2599999aee3cfdec450f8dec72dcd070881
Xapsiel <ilyas_useinov3301@mail.ru>
2024-05-06 13:44:53 +0300

    update first case solution

--- first/main.go
+++ first/main.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	first(6)
+	var n int
+	fmt.Scan(&n)
+	str := first(n)
+	fmt.Println(str)
 
 }
 
-func first(n int) {
+func first(n int) string {
 	if 11 <= n%100 && n%100 <= 14 {
-		fmt.Printf("%d компьютеров", n)
+		return fmt.Sprintf("%d компьютеров", n)
 	} else if n%10 == 1 {
-		fmt.Printf("%d компьютер", n)
+		return fmt.Sprintf("%d компьютер", n)
 	} else if 2 <= n%10 && n%10 <= 4 {
-		fmt.Printf("%d компьютера", n)
+		return fmt.Sprintf("%d компьютера", n)
 	} else {
-		fmt.Printf("%d компьютеров", n)
+		return fmt.Sprintf("%d компьютеров", n)
 	}
 }
